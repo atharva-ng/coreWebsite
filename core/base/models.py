65f0bcbd31262a994abcdb8f886f4fd9f6abfe06
Atharva Ghadi <atharva_main@Atharvas-MacBook-Pro-2.local>
2023-11-11 15:02:58 +0530

    Added coordi model

--- core/base/models.py
+++ core/base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class cordi(models.Model):
+    name = models.TextField()
+    linkdn = models.TextField()
+    photo = models.ImageField()
+    intro = models.CharField(max_length=300)
