b6e718abdb58da314ddb4ef6abcb95360091fa8f
Atharva <atharvaghadi4@gmail.com>
2024-10-16 14:35:35 +0530

    Changed mmultiple to false in scholarship/forms.py

--- core/scholarship/forms.py
+++ core/scholarship/forms.py
@@ -22,7 +22,7 @@ class ApplicationForm(forms.Form):
     supporting_documents = forms.FileField(
         required=False,
         widget=forms.FileInput(attrs={
-            'multiple': True,
+            'multiple': False,
             'class': 'mt-1 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100'
         })
     )
