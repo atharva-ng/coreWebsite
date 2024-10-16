3d729fb155c3e8bde18ca4056a2bf7e0b843c769
paarthsikka <paarthsikka18@gmail.com>
2024-10-16 14:01:34 +0530

    SIA

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# core/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,13 +7,14 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include("base.urls")),
     path('campusVisit/', include("campusVisit.urls")),
+    path('scholarship/', include("scholarship.urls")),  # Add this line to include the scholarship URLs
     path('arcAdmin8879/', admin.site.urls),
-]+static(
+] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
 
 admin.site.index_title = "BITS Alumni Relations"
 admin.site.site_header = "Alumni Control Admin"
-admin.site.site_title = "ALumni Control Admin"
+admin.site.site_title = "Alumni Control Admin"
 
 handler404 = 'base.views.handeling_404'
